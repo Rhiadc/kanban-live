bc2da13b54ab60deb472ff4c4bce85ae20966c32
Rhiad Ciccoli <rhiad.ciccoli@gmail.com>
2021-09-04 22:51:08 -0300

    add create new card, update card

--- lib/livekanban/card.ex
+++ lib/livekanban/card.ex
@@ -2,6 +2,8 @@ defmodule Livekanban.Card do
   use Ecto.Schema
   import Ecto.Changeset
 
+  alias Livekanban.{Repo, Card}
+
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
   schema "cards" do
@@ -15,5 +17,20 @@ defmodule Livekanban.Card do
     card
     |> cast(attrs, [:content])
     |> validate_required([:content])
+  end
+
+  def update(id, changes) do
+    with card when not is_nil(card) <- Repo.get(Card, id),
+      {:ok, card} <- do_update(card, changes) do
+        {:ok, card |> Repo.preload(column: :board)}
+      else
+        {:error, reason} -> {:error, reason}
+      end
+  end
+
+  defp do_update(card, changes) do
+    card
+    |> changeset(changes)
+    |> Repo.update()
   end
 end
--- lib/livekanban_web/router.ex
+++ lib/livekanban_web/router.ex
@@ -21,9 +21,10 @@ defmodule LivekanbanWeb.Router do
   end
 
   # Other scopes may use custom stacks.
-  # scope "/api", LivekanbanWeb do
-  #   pipe_through :api
-  # end
+  scope "/api", LivekanbanWeb do
+    pipe_through :api
+
+  end
 
   # Enables LiveDashboard only for development
   #
